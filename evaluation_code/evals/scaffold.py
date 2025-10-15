db6a923ed1e9aa6af3a483fc08664b971178d318
Diego Canez <canezdiego@gmail.com>
2025-10-15 22:12:24 +0200

    feat: update scaffold for wandb

--- evaluation_code/evals/scaffold.py
+++ evaluation_code/evals/scaffold.py
@@ -6,16 +6,17 @@
 #
 
 import importlib
-
+import wandb
 from src.utils.logging import get_logger
 
 logger = get_logger("Eval runner scaffold")
 
 
 def main(eval_name, args_eval, resume_preempt=False):
-    logger.info(f"Running evaluation: {eval_name}")
-    if eval_name.startswith("app."):
-        import_path = f"{eval_name}.eval"
-    else:
-        import_path = f"evals.{eval_name}.eval"
-    return importlib.import_module(import_path).main(args_eval=args_eval, resume_preempt=resume_preempt)
+    with wandb.init() as run:
+        logger.info(f"Running evaluation: {eval_name}")
+        if eval_name.startswith("app."):
+            import_path = f"{eval_name}.eval"
+        else:
+            import_path = f"evals.{eval_name}.eval"
+        return importlib.import_module(import_path).main(args_eval=args_eval, resume_preempt=resume_preempt)
